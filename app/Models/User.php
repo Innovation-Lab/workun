270db00efa0b5bdd39e7ff28dea18dfbe4e4bb32
zuka <r.iiduka@soushin-lab.co.jp>
2024-07-24 15:35:29 +0900

    [fix]不要項目を削除

--- app/Models/User.php
+++ app/Models/User.php
@@ -103,11 +103,6 @@ class User extends Authenticatable
         return $this->belongsTo(Employment::class);
     }
 
-    protected function scopeOrganization($query)
-    {
-        return $query;
-    }
-
     protected function getAnswerSelfCheckSheetsAttribute()
     {
         return SelfCheckSheet::query()
