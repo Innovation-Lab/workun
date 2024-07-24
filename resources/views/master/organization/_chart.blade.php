62c02e3c1130b9f4b9a29407b1eb4b34dbb55595
Harima <y.harima@soushin-lab.co.jp>
2024-07-24 13:19:18 +0900

    [fix]組織図に従業員リストポップアップ

--- resources/views/master/organization/_chart.blade.php
+++ resources/views/master/organization/_chart.blade.php
@@ -72,6 +72,11 @@
       <div class="p-organizationChart__field">
         <div class="p-organizationChart__item">
           <p class="name"><?= $organization['name']; ?></p>
+          <div class="button__area">
+            <div class="button">
+              <svg width="10" height="10"><use xlink:href="#organizational_user_list" /></svg>
+            </div>
+          </div>
         </div>
       </div>
     <?php } ?>
@@ -110,5 +115,39 @@
 <div class="p-organizationChart">
   <div class="p-organizationChart__layer layer1">
     <?php displayOrganization($organizationChart); ?>
+  </div>
+</div>
+<!--  -->
+<div class="p-organizationChart__userList">
+  <div class="item">
+    <div class="close">×</div>
+    <div class="head u-align between">
+      <p class="title c-txt__md c-txt__weight--600">「賃貸営業部」従業員数</p>
+      <a href="{{route('master.organization.edit_link_employee')}}" class="c-button--text">編集</a>
+    </div>
+    <div class="count">
+      <span class="unit">全</span>
+      <p class="number">24</p>
+      <span class="unit">名</span>
+    </div>
+    <div class="body u-p0">
+      <div class="c-scroll">
+        <ul>
+          <?php  for($p = 0; $p < 24; $p++){ ?>
+            <li>
+              <div class="p-user">
+                <div class="p-user__image c-noImage">
+                  <img class="c-image c-image--round" src="">
+                </div>
+                <div class="p-user__text">
+                  <div class="name">酒井 雄輝</div>
+                </div>
+              </div>
+              <p class="position">代表取締役社長</p>
+            </li>
+          <?php } ?> 
+        </ul>
+      </div>
+    </div>
   </div>
 </div>
