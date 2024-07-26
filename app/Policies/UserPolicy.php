c9eedddd7962bb1e32b5b8beff5bdb9f6ed1a38e
横枕尚樹 <n.yokomakura@soushin-lab.co.jp>
2024-07-26 10:27:59 +0900

    [fix]ポリシー修正

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -17,9 +17,9 @@ class UserPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user): bool
+    public function view(User $auth_user, User $user): bool
     {
-        return $user->organization_id === $user->organization_id;
+        return $user->organization_id === $auth_user->organization_id;
     }
 
     /**
@@ -33,33 +33,32 @@ class UserPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user): bool
+    public function update(User $auth_user, User $user): bool
     {
-        return $this->view($user) &&
-            $user->count() === 0;
+        return $this->view($auth_user, $user);
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user): bool
+    public function delete(User $auth_user, User $user): bool
     {
-        return $this->update($user);
+        return $this->update($auth_user, $user);
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user): bool
+    public function restore(User $auth_user, User $user): bool
     {
-        return $this->update($user);
+        return $this->update($auth_user, $user);
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user): bool
+    public function forceDelete(User $auth_user, User $user): bool
     {
-        return $this->update($user);
+        return $this->update($auth_user, $user);
     }
 }
